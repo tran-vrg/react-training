8fc4fde5bbb89d482e01e9dbde40182defe60cf5
Tuan Dung TRAN <dung.tran@vrg.jp>
2018-07-18 23:52:32 +0900

    example of setting things up

--- public/src/index.js
+++ public/src/index.js
@@ -1,1 +1,8 @@
 // root react
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Layout from './components/layout';
+
+render(
+  <Layout />, document.getElementById('app')
+);
